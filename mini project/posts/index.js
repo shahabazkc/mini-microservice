fc2a2180ea17a67a1087ced2d351cbc486ea35d7
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-03-27 11:39:21 +0530

    Added: event bus and posts service with kubernetes connection created

--- mini project/posts/index.js
+++ mini project/posts/index.js
@@ -13,27 +13,34 @@ app.use(express.urlencoded({ extended: false }));
 const posts = {};
 
 app.get('/posts', (req, res) => {
-  res.send(posts);
+    res.send(posts);
 })
 
-app.post("/posts", async(req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
-  posts[id] = {id, title};
-  await axios.post('http://localhost:4005/events',{
-    type:"PostCreated",
-    data:{
-      id,title
-    }
-  });
-  res.status(201).send(posts[id]);
+app.post("/posts", async (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = { id, title };
+
+    await axios.post('http://event-bus-srv:4005/events', {
+        type: "PostCreated",
+        data: {
+            id,
+            title
+        }
+    }).then((response) => {
+        console.log(response.data);
+    }).catch((err) => {
+        console.log("error is in sending event bus request ");
+    });
+
+    res.status(201).send(posts[id]);
 });
 
-app.post('/events',(req,res)=>{
-  console.log("Recieved Event ",req.body.type);
-  res.send({})
+app.post('/events', (req, res) => {
+    console.log("Recieved Event ", req.body.type);
+    res.send({})
 })
 
 app.listen(4000, () => {
-  console.log("listening on port 4000");
+    console.log("listening on port 4000");
 })
