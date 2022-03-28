b89fa63fed1e868b481c19445e6db911e7ce312f
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-03-28 19:46:13 +0530

    kubernetes synchronised

--- mini project/event-bus/index.js
+++ mini project/event-bus/index.js
@@ -11,12 +11,12 @@ app.post("/events", (req, res) => {
     axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
         console.log(err.message);
     });
-    // axios.post("http://localhost:4001/events", event).catch((err) => {
-    //   console.log(err.message);
-    // });
-    // axios.post("http://localhost:4002/events", event).catch((err) => {
-    //   console.log(err.message);
-    // });
+    axios.post("http://comments-srv:4001/events", event).catch((err) => {
+      console.log(err.message);
+    });
+    axios.post("http://query-srv:4002/events", event).catch((err) => {
+      console.log(err.message);
+    });
     res.send({ status: "OK" });
 });
 
